ec996997b0a274177a6bb56827dbbf7da5cb7fdf
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-11-09 17:35:02 +0200

    Created IConsole. Added methods DisplayMainMenu(), DisplayMenu(), DisplayBackToMainMenu()

---
+++ IOValidationLibrary/Interfaces/IConsole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IOValidation.Interfaces
+{
+    interface IConsole
+    {
+        void DisplayMainMenu();
+        void DisplayMenu(string title, string description);
+        void DisplayBackToMainMenu();
+    }
+}
